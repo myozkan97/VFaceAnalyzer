2423b31b8e596e5429b83a96b7a54aba6bd62caa
myozkan <myozkan@yandex.com>
2020-05-20 05:05:07 +0300

    switched to @serengil s pretrained models to implement gender and age predictions.

--- Gotcha.py
+++ Gotcha.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 import pafy
 import imutils
+import models
+import keras
 
 # url of the video to predict Age and gender
-url = 'https://www.youtube.com/watch?v=iq2_gTETBXM'
+url = 'https://www.youtube.com/watch?v=cZ-11eGOkKM'
 vPafy = pafy.new(url)
 play = vPafy.getbest(preftype="mp4")
 cap = cv2.VideoCapture(play.url)
@@ -14,19 +16,26 @@ age_list = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)', '(25, 32)', '(38, 43)', '
 gender_list = ['Male', 'Female']
 
 
+age_model = models.ageModel()
+gender_model = models.genderModel()
+
+
+#age model has 101 outputs and its outputs will be multiplied by its index label. sum will be apparent age
+output_indexes = np.array([i for i in range(0, 101)])
+
 def load_caffe_models():
     age_net = cv2.dnn.readNetFromCaffe('deploy_age.prototxt', 'age_net.caffemodel')
     gender_net = cv2.dnn.readNetFromCaffe('deploy_gender.prototxt', 'gender_net.caffemodel')
-    return (age_net, gender_net)
+    return age_net, gender_net
 
 
-def video_detector(age_net, gender_net):
+def video_detector():
     font = cv2.FONT_HERSHEY_SIMPLEX
     while True:
         for i in range(25):
             cap.read()
         ret, image = cap.read()
-        image = imutils.resize(image, width=520)
+        image = imutils.resize(image, width=1024)
 
         face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -35,28 +44,55 @@ def video_detector(age_net, gender_net):
         if (len(faces) > 0):
             print("Found {} faces".format(str(len(faces))))
         for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
-            # Get Face
-            face_img = image[y:y + h, h:h + w].copy()
-            blob = cv2.dnn.blobFromImage(face_img, 1, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-            # Predict Gender
-            gender_net.setInput(blob)
-            gender_preds = gender_net.forward()
-            gender = gender_list[gender_preds[0].argmax()]
-            print("Gender : " + gender)
-            # Predict Age
-            age_net.setInput(blob)
-            age_preds = age_net.forward()
+            if w > 20:
 
-            i = age_preds[0].argmax()
-            confidence = age_preds[0][i]
-            if confidence > 0.85:
-                age = age_list[i]
-            else:
-                age = "?"
-            print("Age Range: " + age)
-            overlay_text = "%s %s" % (gender, age)
-            cv2.putText(image, overlay_text, (x, y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+                # Get Face
+                face_img = image[y:y + h, h:h + w].copy()
+
+
+                # age gender data set has 40% margin around the face. expand detected face.
+                margin = 30
+                margin_x = int((w * margin) / 100);
+                margin_y = int((h * margin) / 100)
+
+                detected_face = image[int(y):int(y + h), int(x):int(x + w)].copy()
+
+
+                try:
+                    detected_face = image[int(y - margin_y):int(y + h + margin_y),
+                                int(x - margin_x):int(x + w + margin_x)]
+                    cv2.imshow("face_img", detected_face)
+                except:
+                    print("Detected face has not enough margin around to predict gender and age")
+
+
+                try:
+                    detected_face = cv2.resize(detected_face, (224, 224))
+
+                    img_pixels = keras.preprocessing.image.img_to_array(detected_face)
+                    img_pixels = np.expand_dims(img_pixels, axis=0)
+                    img_pixels /= 255
+
+                    # Predict age and gender
+                    age_dists = age_model.predict(img_pixels)
+                    apparent_age = str(int(np.floor(np.sum(age_dists * output_indexes, axis =1))[0]))
+
+                    gender_distribution = gender_model.predict(img_pixels)[0]
+                    gender_index = np.argmax(gender_distribution)
+
+                    if gender_index == 0:
+                        gender = "F"
+                    else:
+                        gender = "M"
+
+                    # Create an overlay text and put it into frame
+                    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+                    overlay_text = "%s %s" % (gender, apparent_age)
+                    cv2.putText(image, overlay_text, (x, y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                except Exception as e:
+                    print("exception ", e)
+
             cv2.imshow('frame', image)
             # 0xFF is a hexadecimal constant which is 11111111 in binary.
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -64,5 +100,4 @@ def video_detector(age_net, gender_net):
 
 
 if __name__ == "__main__":
-    age_net, gender_net = load_caffe_models()
-video_detector(age_net, gender_net)
+    video_detector()
---
+++ models.py
@@ -0,0 +1,87 @@
+from keras.models import Model, Sequential
+from keras.layers import Input, Convolution2D, ZeroPadding2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
+
+
+def loadVggFaceModel():
+    model = Sequential()
+    model.add(ZeroPadding2D((1, 1), input_shape=(224, 224, 3)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(Convolution2D(4096, (7, 7), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(4096, (1, 1), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(2622, (1, 1)))
+    model.add(Flatten())
+    model.add(Activation('softmax'))
+
+    return model
+
+
+def ageModel():
+    model = loadVggFaceModel()
+
+    base_model_output = Sequential()
+    base_model_output = Convolution2D(101, (1, 1), name='predictions')(model.layers[-4].output)
+    base_model_output = Flatten()(base_model_output)
+    base_model_output = Activation('softmax')(base_model_output)
+
+    age_model = Model(inputs=model.input, outputs=base_model_output)
+
+    # you can find the pre-trained weights for age prediction here:
+    # https://drive.google.com/file/d/1YCox_4kJ-BYeXq27uUbasu--yz28zUMV/view?usp=sharing
+    age_model.load_weights("age_model_weights.h5")
+
+    return age_model
+
+
+def genderModel():
+    model = loadVggFaceModel()
+
+    base_model_output = Sequential()
+    base_model_output = Convolution2D(2, (1, 1), name='predictions')(model.layers[-4].output)
+    base_model_output = Flatten()(base_model_output)
+    base_model_output = Activation('softmax')(base_model_output)
+
+    gender_model = Model(inputs=model.input, outputs=base_model_output)
+
+    # you can find the pre-trained weights for gender prediction here:
+    # https://drive.google.com/file/d/1wUXRVlbsni2FN9-jkS_f4UTUrm1bRLyk/view?usp=sharing
+    gender_model.load_weights("gender_model_weights.h5")
+
+    return gender_model
+
+
